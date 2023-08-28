31e6343bc40235fe22376a51613b84576df30e61
Hylke Nicola <hyskon@outlook.com>
2023-08-28 14:48:06 +0200

    changes1

--- snowflakeConsumerTrigger/__init__.py
+++ snowflakeConsumerTrigger/__init__.py
@@ -4,49 +4,36 @@ import azure.functions as func
 import snowflake.connector
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
+from snowflake.connector import connect
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # Check if the HTTP method is a POST request
-    if req.method == 'POST':
-        # Initialize SecretClient with the managed identity
+    try:
+        # Verkrijg toegang tot Azure Key Vault
+        keyvault_name = "snowflakeauthentication"
         credential = DefaultAzureCredential()
-        secret_client = SecretClient(vault_url='https://snowflakeauthentication.vault.azure.net/', credential=credential)
-        
-        # Snowflake connection parameters
-        account = secret_client.get_secret('SnowflakeAccount').value
-        user = secret_client.get_secret('SnowflakeUsername').value
-        password = secret_client.get_secret('SnowflakePassword').value
-        # TODO: Real life scenario required below
-        warehouse = 'WH_COMPUTE'
-        database = 'TEST_API'
-        schema = 'INVENTORY'
-        view = 'EXAMPLE_VIEW' 
-        
-        # Create a Snowflake connection
-        conn = snowflake.connector.connect(
-            user=user,
-            password=password,
-            account=account,
-            warehouse=warehouse,
-            database=database,
-            schema=schema
-        )
-        
-        # Execute a sample query
-        cursor = conn.cursor()
-        query = f"SELECT * FROM {view}"
-        cursor.execute(query)
-        
-        # Fetch and process query results
-        results = cursor.fetchall()
-        for row in results:
-            # Process and send data to "work tray" service (you can add this logic here)
-            print(row)
-        
-        # Close the connection
-        cursor.close()
-        conn.close()
+        secret_client = SecretClient(vault_url=f"https://{keyvault_name}.vault.azure.net", credential=credential)
 
-        return func.HttpResponse("Data processed successfully.", status_code=200)
-    else:
-        return func.HttpResponse("Invalid HTTP method.", status_code=400)
+        # Haal Snowflake-inloggegevens uit Key Vault
+        snowflake_account_url = secret_client.get_secret("SnowflakeAccount").value
+        snowflake_username = secret_client.get_secret("SnowflakeUser").value
+        snowflake_password = secret_client.get_secret("SnowflakePassword").value
+
+        # Maak verbinding met Snowflake
+        con = connect(
+            user=snowflake_username,
+            password=snowflake_password,
+            account=snowflake_account_url,
+            warehouse="WH_COMPUTE",
+            database="TEST_API",
+            schema="INVENTORY"
+        )
+
+        # Voer een Snowflake-query uit
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM PRODUCTS")
+        results = cursor.fetchall()
+
+        return func.HttpResponse(f"Query results: {results}")
+
+    except Exception as e:
+        return func.HttpResponse(f"An error occurred: {str(e)}", status_code=500)
